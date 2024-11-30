f4f776729a519888099f35b8e6e956ec2083497d
mizotii <mztiiiii0@gmail.com>
2024-11-29 17:18:55 -0800

    init

---
+++ src/test/java/GraphPackage/VertexTest.java
@@ -0,0 +1,80 @@
+package GraphPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VertexTest {
+
+    @Test
+    void getLabel() {
+    }
+
+    @Test
+    void hasPredecessor() {
+    }
+
+    @Test
+    void setPredecessor() {
+    }
+
+    @Test
+    void getPredecessor() {
+    }
+
+    @Test
+    void visit() {
+    }
+
+    @Test
+    void unvisit() {
+    }
+
+    @Test
+    void isVisited() {
+    }
+
+    @Test
+    void getCost() {
+    }
+
+    @Test
+    void setCost() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getWeightIterator() {
+    }
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void testConnect() {
+    }
+
+    @Test
+    void getNeighborIterator() {
+    }
+
+    @Test
+    void hasNeighbor() {
+    }
+
+    @Test
+    void getUnvisitedNeighbor() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void display() {
+    }
+}
