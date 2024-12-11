087478ea73ef52ae756ef526d0688974879d828f
ejnorman <apericnorman@gmail.com>
2024-12-10 20:30:39 -0800

    fixed some issues in menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import ADTPackage.ListWithIteratorInterface;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,19 +8,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //set up existing network
         ProfileManager network = setUpNetwork();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your username: ");
-        String username = sc.nextLine();
-        System.out.println("Choose an element: Earth: earth \nWind: wind \nFire: fire \nWater: water");
-        String avatar = "src/avatars/" + sc.nextLine() + ".png";
+        //get new user information
+        String username, avatar;
+        while (true) {
+            System.out.println("Enter your username: ");
+            username = sc.nextLine();
+            if (username.isEmpty()) {
+                System.out.println("Username cannot be empty");
+            }
+            else if (network.containsProfile(username)) {
+                System.out.println("Username is already in use");
+            }
+            else {
+                break;
+            }
+        }
+        while (true) {
+            System.out.println("Choose an element: \nEarth: earth \nWind: wind \nFire: fire \nWater: water");
+            String input = sc.nextLine();
+            if (!input.equals("earth") && !input.equals("wind") && !input.equals("fire") && !input.equals("water")) {
+                System.out.println("Invalid input");
+            }
+            else {
+                avatar = "src/avatars/" + input + ".png";
+                break;
+            }
+        }
         Profile user = new Profile(username, avatar, "Online");
         network.addProfile(user);
-        Profile currentUser = user;
+        Profile currentUser = user; //keep track of current user
+
         String actionList = "Select an action: \nModify Current Profile: m \nView All Profiles: v " +
                 "\nAdd Friend: af \nView Friend List: vfl \nView Friend's Friend List: vffl " +
                 "\nDelete Profile: d \nAdd Profile: a \nSwitch Current User: s \nEnd Session: end";
         System.out.println(actionList);
+        //menu (could have been a switch statement, but I don't want to mess with it)
         while (true) {
             String action = sc.nextLine();
             if (action.equals("m")) {
@@ -30,7 +57,7 @@ public class Main {
             } else if (action.equals("vfl")) {
                 viewFriendList(currentUser);
             } else if (action.equals("vffl")) {
-                viewFriendsFriendList(user, sc);
+                viewFriendsFriendList(user, network, sc);
             } else if (action.equals("d")) {
                 network = deleteProfile(currentUser, network, sc);
             } else if (action.equals("a")) {
@@ -45,17 +72,17 @@ public class Main {
                 System.out.println("Invalid action");
             }
             if (!action.equals("c")) {
-                System.out.println("Select a new action: (type 'c' to pull up actions list)");
+                System.out.println("\nSelect a new action: (type 'c' to pull up actions list)");
             }
         }
     }
 
     public static ProfileManager setUpNetwork() {
         ProfileManager network = new ProfileManager();
-        Profile ejnorman = new Profile("ejnorman", "src/avatars/E.png", "Online");
-        Profile mztii = new Profile("mztii", "src/avatars/M.png", "Online");
-        Profile seiyaroo = new Profile("seiyaroo", "src/avatars/S.png", "Online");
-        Profile pbriant = new Profile("pbriant", "src/avatars/P.png", "Online");
+        Profile ejnorman = new Profile("ejnorman", "src/avatars/earth.png", "Online");
+        Profile mztii = new Profile("mztii", "src/avatars/water.png", "Online");
+        Profile seiyaroo = new Profile("seiyaroo", "src/avatars/wind.png", "Online");
+        Profile pbriant = new Profile("pbriant", "src/avatars/fire.png", "Online");
         network.addProfile(ejnorman);
         network.addProfile(mztii);
         network.addProfile(seiyaroo);
@@ -77,14 +104,28 @@ public class Main {
                     String newUsername = sc.nextLine();
                     if (network.containsProfile(newUsername)) {
                         System.out.println("Username is taken");
-                    } else {
+                    }
+                    else if (newUsername.isEmpty()) {
+                        System.out.println("Username cannot be empty");
+                    }
+                    else {
                         user.setUsername(newUsername);
                         break;
                     }
                 }
             }
             else if (action.equals("a")) {
-                System.out.println("Pick an avatar: \n");
+                while (true) {
+                    System.out.println("Choose an element: \nEarth: earth \nWind: wind \nFire: fire \nWater: water");
+                    String input = sc.nextLine();
+                    if (!input.equals("earth") && !input.equals("wind") && !input.equals("fire") && !input.equals("water")) {
+                        System.out.println("Invalid input");
+                    }
+                    else {
+                        user.setAvatar("src/avatars/" + input + ".png");
+                        break;
+                    }
+                }
             }
             else if (action.equals("s")) {
                 System.out.println("Change status: \nOnline: online \nAway: away " +
@@ -126,17 +167,21 @@ public class Main {
         }
     }
 
-    public static void viewFriendsFriendList(Profile user, Scanner sc) {
+    public static void viewFriendsFriendList(Profile user, ProfileManager network, Scanner sc) {
         ArrayList<Profile> friends = user.getFriends();
         System.out.println("Enter your friend's name: ");
         String friendName = sc.nextLine();
+        if (!network.containsProfile(friendName)) {
+            System.out.println("User not found");
+            return;
+        }
         for (Profile friend : friends) {
             if (friend.getUsername().equals(friendName)) {
                 viewFriendList(friend);
                 return;
             }
         }
-        System.out.println("User not found");
+        System.out.println("User is not your friend");
     }
 
     public static ProfileManager deleteProfile(Profile user, ProfileManager network, Scanner sc) {
@@ -164,13 +209,25 @@ public class Main {
             if (network.containsProfile(profileName)) {
                 System.out.println("Username already exists");
             }
+            else if (profileName.isEmpty()) {
+                System.out.println("Username cannot be empty ");
+            }
             else {
-                System.out.println("Avatar: ");
-                avatar = sc.nextLine();
+                while (true) {
+                    System.out.println("Choose an element: \nEarth: earth \nWind: wind \nFire: fire \nWater: water");
+                    String input = sc.nextLine();
+                    if (!input.equals("earth") && !input.equals("wind") && !input.equals("fire") && !input.equals("water")) {
+                        System.out.println("Invalid input");
+                    }
+                    else {
+                        avatar = "src/avatars/" + input + ".png";
+                        break;
+                    }
+                }
                 break;
             }
         }
-        Profile newUser = new Profile(profileName, avatar, "Online");
+        Profile newUser = new Profile(profileName, "src/avatars/" + avatar + ".png", "Online");
         network.addProfile(newUser);
         return network;
     }
