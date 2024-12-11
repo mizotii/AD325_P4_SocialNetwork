4df00ccb6a7d4d05ee7b07bbfbcead3857adaaaa
mizotii <mztiiiii0@gmail.com>
2024-12-10 16:03:38 -0800

    test removeFriendship properly, all display methods now pass

--- src/test/java/ProfileManagerTest.java
+++ src/test/java/ProfileManagerTest.java
@@ -1,4 +1,4 @@
-// Joel Yang: created all unit tests for this class.
+// Joel Yang: created all unit tests for this class unless marked otherwise.
 
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +19,14 @@ class ProfileManagerTest {
     void removeProfile() {
         m = new ProfileManager();
         Profile p = new Profile("turtle", "turtle.png", "Online");
+        Profile p2 = new Profile("goose", "goose.png", "Online");
+        Profile p3 = new Profile("DOG", "DOG.png", "Online");
         m.addProfile(p);
+        m.addProfile(p2);
+        m.addProfile(p3);
+        m.addFriendship(p, p2);
+        m.addFriendship(p2, p3);
+        m.addFriendship(p, p3);
         assertTrue(m.removeProfile(p));
     }
 
@@ -49,10 +56,9 @@ class ProfileManagerTest {
                 "Name: goose\r\n" +
                 "Avatar: goose.png\r\n" +
                 "Status: Online\r\n" +
-                "\r\n" +
                 "Friends: \r\n" +
-                "turtle\r\n" +
-                "Name: turtle\r\nAvatar: turtle.png\r\nStatus: Online\r\n\r\nFriends: \r\ngoose\r\n");
+                "turtle \r\n" +
+                "Name: turtle\r\nAvatar: turtle.png\r\nStatus: Online\r\nFriends: \r\ngoose \r\n");
         System.setOut(originalOut);
     }
 
@@ -72,10 +78,9 @@ class ProfileManagerTest {
                 "Name: goose\r\n" +
                         "Avatar: goose.png\r\n" +
                         "Status: Online\r\n" +
-                        "\r\n" +
                         "Friends: \r\n" +
-                        "turtle\r\n" +
-                        "Name: turtle\r\nAvatar: turtle.png\r\nStatus: Online\r\n\r\nFriends: \r\ngoose\r\n");
+                        "turtle \r\n" +
+                        "Name: turtle\r\nAvatar: turtle.png\r\nStatus: Online\r\nFriends: \r\ngoose \r\n");
         System.setOut(originalOut);
     }
 
